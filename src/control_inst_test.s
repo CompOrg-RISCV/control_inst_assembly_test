0d3e29879330d867c34bfb8d2eaac29a0a25f689
CompOrg-RISCV <114032938+CompOrg-RISCV@users.noreply.github.com>
2022-10-29 17:20:54 -0700

    Update control_inst_test.s

--- src/control_inst_test.s
+++ src/control_inst_test.s
@@ -15,21 +15,24 @@ _start:
 /*********************************************************************************
  * Simple branch test including tests of clearing instruction after branch taken
  *********************************************************************************/
-	addi x2, x0, 2			// Load 2 into x2 reg, 2 + 0 = 2
+	addi x2, x0, 2			        // Load 2 into x2 reg, 2 + 0 = 2
 	nop
 	nop
 	nop
-	beq x0, x2, FAIL		// x0=0, x2=2, so it should not branch
-	addi x3, x0, 1			// check that x2 has been updated with the value 2 //x3 should be set to 1
+	beq x0, x2, FAIL		        // x0=0, x2=2, so it should not branch
+	addi x3, x0, 1		    // check that x2 has been updated with the value 2 //x3 should be set to 1
 	nop
 	nop
 	nop
-	beq x0, x0, PASS1		// x=0, x=0, so branch to PASS1
-	addi x4, x0, 1			// x3 = 1 // if x4 gets set to 1, the EX instruction was not cleared (NOP)
+	beq x0, x0, PASS1		        // x=0, x=0, so branch to PASS1
+	addi x4, x0, 1			// x3 = 1 
 	nop
 	nop
 	nop
-	halt					// If halt occurs, brnach instruction failed
+    nop
+    nop                     // if x4 gets set to 1, the EX instruction was not cleared (NOP)
+    nop
+	halt					// If halt reached, branch instruction failed
 /**************************************************************************
  * If reached this halt statement, test failure
  **************************************************************************/
@@ -40,24 +43,31 @@ _start:
 	nop
 	nop
 PASS2:
-	nop
-	nop
+	nop                     // x3 should equal to 1 since the branch beforehand was not taken
+ 	nop						// check here to ensure that x4 and x5 are not set, if set,
+	nop						//      instruction in ID or EX stage was not cleared (NOP)
+	nop						// If halts here, both positive and negative offset branches successful
 	nop
 	nop
 	halt
 /**************************************************************************
  * Simple Jump instruction tests
  **************************************************************************/
- 	nop						// x3 should equal to 1 since the branch beforehand was not taken
- 	nop						// check here to ensure that x4 and x5 are not set, if set,
-	nop						//      instruction in ID or EX stage was not cleared (NOP)
-	nop						// If halts here, both positive and negative branches successful
+ 	nop						
+ 	nop						
+	nop						
+	nop						
 	nop
 	nop
-	addi x2, x0, (RETURN1 & 0xfff)					// Save lower 12-bit return address in x2
+	addi x2, x0, (RETURN1 & 0xff)		// Save lower 16-bit address.  Small program less than 16-bits address
+    addi x30, x0, ((RETURN1 & 0xff00) >> 8)
+    slli x30, x30, 8
+    or x2, x2, x30
+    nop
 	nop
 	nop
 	nop
+    nop                     // x2 = RETURN1 address
 	jal x1, JUMP1			// jump to positive offset and save return address, RETURN1 label into x1
 RETURN1:
 	nop
@@ -90,7 +100,7 @@ JUMP2:
 	nop
 	nop
 	nop
-	nop
+	nop                 
 	halt					// if this halt is reached, there is an error in your jump routine
 /**************************************************************************
  * If reached this halt statement, test failure
@@ -111,7 +121,7 @@ PASS1:
 	nop
 	nop
 	nop
-	halt					// reaching this break indicates a failure to branch on line 104
+	halt					// reaching this break indicates a failure to branch proceeding this halt
 /**************************************************************************
  * Extra-credit Load test to validate structural hazard detected and stall
  **************************************************************************/
@@ -125,13 +135,14 @@ JUMP1:
 	addi x10, x0, 1			// Set x10 to 1 to validate jump to address is executed
 	nop
 	addi x3, x0, 0xff
-	slli x3, x3, 4
-	addi x3, x3, 0xf		// Set x3 to the mask of 0xfff
-	and x3, x3, x1			// Save lower 12-bits of return address to compare with lower 12-bits of label
+    slli x30, x3, 8
+	or x3, x3, x30      // Save lower 16-bits of return address to compare with lower 16-bits of label
+	and x3, x3, x1		
+	nop                 // if x10 does not equal 1, the target jump instruction is not executed
+    nop                 //     - possible error could be jumping to PC + 4 + jump offset instead of
+	nop                 //     - PC + jump offset
 	nop
-	nop
-	nop
-	nop
+	nop                 // x3 = return address saved
 	nop
 	bne	x3, x2, FAIL		// x2 = RETURN1.  Branch if not equal to fail
 	nop
